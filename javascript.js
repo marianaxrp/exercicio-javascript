f3a73bc44d0a2b234349a70432788720db1794ce
Mariana Xavier <marianaxrp@gmail.com>
2019-04-24 00:01:23 -0300

    exercicios de javascript resolvidos

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let nome, apelido;
+
+nome = "Mariana";
+console.log(nome);
+
+apelido = "Mari";
+console.log(apelido);
+
+
+let minhaLista =["arroz", "feijão", "bife", "batata", "ovo"];
+
+console.log(minhaLista);
+console.log(minhaLista[1]);
+console.log(minhaLista.length);
+
+minhaLista[2] = "coxinha";
+console.log(minhaLista);
